6667a088b36f192c84be11fa57cbfd6f937c4d25
Werner Bessinger <vanbess@users.noreply.github.com>
2020-04-24 13:17:49 +0200

    Class and js setup

---
+++ classes/SBSI_Back.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 
+ */
+class SBSI_Back
+{
+
+    /**
+     * Class init
+     */
+    public static function init()
+    {
+        # code...
+    }
+
+}
+
+SBSI_Back::init();
---
+++ classes/SBSI_Front.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * 
+ */
+class SBSI_Front
+{
+
+    public static function init()
+    {
+        # code...
+    }
+}
+
+SBSI_Front::init();
